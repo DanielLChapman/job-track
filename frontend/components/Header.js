a13b65601b15133530526b433e361661e474788b
DanielLChapman <danielc@my.ccsu.edu>
2022-07-06 16:02:20 -0400

    Modal window updates + rract-transition-group

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -5,6 +5,7 @@ import SignOut from "./Signout";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Modal from "./Modal";
+import { CSSTransition } from "react-transition-group";
 
 Header.propTypes = {
     user: PropTypes.object,
@@ -13,13 +14,13 @@ Header.propTypes = {
 function Header({ user }) {
     const [modal, setModal] = useState(false);
     const [modalCreateJob, setModalCreateJob] = useState(false);
+    const [inUp, setInUp] = useState("In");
 
     return (
         <>
             <h1>Job Track</h1>
             {user && (
                 <div className="header-button-class">
-
                     <button
                         type="button"
                         className="sign-in-button"
@@ -40,23 +41,57 @@ function Header({ user }) {
             )}
             {!user && (
                 <div className="header-button-class">
-                <button
-                    type="button"
-                    className="sign-in-button"
-                    onClick={() => {
-                        setModal(!modal);
-                    }}
-                >
-                    Sign In / Up
-                </button>
+                    <button
+                        type="button"
+                        className="sign-in-button"
+                        onClick={() => {
+                            setModal(!modal);
+                        }}
+                    >
+                        Sign In / Up
+                    </button>
                 </div>
             )}
-
-            {modal && !user && (
-                <Modal closeFunc={setModal}>
-                    <SignIn closeFunc={setModal} closeValue={!modal} />
-                    <SignUp />
-                </Modal>
+            {!user && (
+                <CSSTransition
+                    in={modal}
+                    timeout={200}
+                    classNames="modal-transition"
+                    unmountOnExit
+                    onExited={() => setModal(false)}
+                >
+                    
+                    <Modal closeFunc={setModal} >
+                        <div className="header">
+                            {inUp === "In" && (
+                                <button
+                                    type="button"
+                                    className="sign-in-up-selector-button"
+                                    onClick={() => {
+                                        setInUp("Up");
+                                    }}
+                                >
+                                    Sign Up
+                                </button>
+                            )}
+                            {inUp === "Up" && (
+                                <button
+                                    type="button"
+                                    className="sign-in-up-selector-button"
+                                    onClick={() => {
+                                        setInUp("In");
+                                    }}
+                                >
+                                    Sign In
+                                </button>
+                            )}
+                        </div>
+                        {inUp === "In" && (
+                            <SignIn closeFunc={setModal} closeValue={!modal} />
+                        )}
+                        {inUp === "Up" && <SignUp />}
+                    </Modal>
+                </CSSTransition>
             )}
         </>
     );
--- frontend/components/Modal.js
+++ frontend/components/Modal.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import { CSSTransition } from "react-transition-group";
 
 function Modal(props) {
     return (
-        <div className='modal-window'>
-            <button type='button' className='close-modal' onClick={() => {
-                props.closeFunc(false);
-            }}>X</button>
-            {props.children}
-        </div>
+        <>
+            <div
+                className="blur-layer"
+                onClick={() => {
+                    props.closeFunc(false);
+                }}
+            ></div>
+            <div className="modal-window">
+                <button
+                    type="button"
+                    className="close-modal"
+                    onClick={() => {props.closeFunc(false);}}
+                >
+                    X
+                </button>
+                {props.children}
+            </div>
+        </>
     );
 }
 
